aae2fb20c8573cb5f279defb0026e4d4aad303ae
JhocepGab <snkje123@gmail.com>
2025-05-22 11:20:53 -0500

    Agregar archivos de proyecto.

---
+++ Sem09_13126/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem09_13126
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            while (true)
+            {
+                Console.WriteLine("Menú:");
+                Console.WriteLine("1. Sumar");
+                Console.WriteLine("2. Restar");
+                Console.WriteLine("3. Multiplicar");
+                Console.WriteLine("4. Dividir");
+                Console.WriteLine("0. Salir");
+                Console.Write("Elige una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                if (opcion == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
